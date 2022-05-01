44709aca8e30ff7e3788e7642958e7dbe140e971
Vladislav <vkoval2002@gmail.com>
2022-05-02 00:48:08 +0300

    Add eureka

--- customer/src/main/kotlin/com/todoapp/customer/CustomerApplication.kt
+++ customer/src/main/kotlin/com/todoapp/customer/CustomerApplication.kt
@@ -1,11 +1,36 @@
 package com.todoapp.customer
 
+import com.todoapp.customer.dao.CustomerRequest
+import com.todoapp.customer.model.Customer
+import com.todoapp.customer.model.Role
+import com.todoapp.customer.service.CustomerService
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class CustomerApplication
+@EnableEurekaClient
+@EnableFeignClients
+class CustomerApplication{
+
+    //create sql file
+    @Bean
+    fun init(customerService: CustomerService) = CommandLineRunner {
+        customerService.saveRole(Role("ROLE_USER"))
+        customerService.saveRole(Role("ROLE_ADMIN"))
+
+        customerService.saveCustomer(CustomerRequest("1","1"))
+        customerService.saveCustomer(CustomerRequest("2","2"))
+        customerService.saveCustomer(CustomerRequest("3","3"))
+        customerService.saveCustomer(CustomerRequest("4","4"))
+
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CustomerApplication>(*args)
 }
+
--- notification/src/main/kotlin/com/todoapp/notification/NotificationApplication.kt
+++ notification/src/main/kotlin/com/todoapp/notification/NotificationApplication.kt
@@ -2,8 +2,13 @@ package com.todoapp.notification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 class NotificationApplication
 
 fun main(args: Array<String>) {
